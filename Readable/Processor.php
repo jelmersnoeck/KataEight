c5a8ad0012757ef4c5556be5f5a773b731b0fa9c
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2013-03-07 20:29:08 +0100

    Readable: initiate processor.

---
+++ Readable/Processor.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Jelmer Snoeck - Kata Eight
+ *
+ * @author    Jelmer Snoeck <jelmer@siphoc.com>
+ * @copyright 2013 Jelmer Snoeck
+ * @link      http://siphoc.com
+ */
+
+namespace JelmerSnoeck\KataEight\Readable;
+
+/**
+ * This class is meant to be the readable code. Here I'll try to make the
+ * program as readable as I can.
+ *
+ * @author Jelmer Snoeck <jelmer@siphoc.com>
+ */
+class Processor
+{
+}
---
+++ Tests/Readable/ProcessorTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Jelmer Snoeck - Kata Eight
+ *
+ * @author    Jelmer Snoeck <jelmer@siphoc.com>
+ * @copyright 2013 Jelmer Snoeck
+ * @link      http://siphoc.com
+ */
+
+namespace JelmerSnoeck\KataEight\Tests\Readable;
+
+use JelmerSnoeck\KataEight\Readable\Processor;
+
+/**
+ * This class is meant to be the readable code. Here I'll try to make the
+ * program as readable as I can.
+ *
+ * @author Jelmer Snoeck <jelmer@siphoc.com>
+ */
+
+class ProcessorTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_it_constructs()
+    {
+        $processor = new Processor;
+    }
+}
