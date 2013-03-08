1a1bc0b45b977a0739a20bc3ac10ab8650ab4c73
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2013-03-08 23:36:52 +0100

    Fast: add basic test.

---
+++ Fast/Processor.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Jelmer Snoeck - Kata Eight
+ *
+ * @author    Jelmer Snoeck <jelmer@siphoc.com>
+ * @copyright 2013 Jelmer Snoeck
+ * @link      http://siphoc.com
+ */
+
+namespace JelmerSnoeck\KataEight\Fast;
+
+/**
+ * This class is meant to be the fast code. Here I'll try to make the program as
+ * fast as I can.
+ *
+ * @author Jelmer Snoeck <jelmer@siphoc.com>
+ */
+class Processor
+{
+}
