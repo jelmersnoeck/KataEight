73ca82ca34699b438a9dba6914feaddc55408aaa
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2013-03-09 21:25:07 +0100

    Extendible: replace dataList by dataProvider.
    I forgot some values in the tests.

--- Tests/Extendible/ProcessorTest.php
+++ Tests/Extendible/ProcessorTest.php
@@ -18,58 +18,58 @@ use JelmerSnoeck\KataEight\Extendible\Processor;
  */
 class ProcessorTest extends \PHPUnit_Framework_TestCase
 {
-    protected $dataList;
+    protected $dataProvider;
 
     public function setUp()
     {
-        $this->dataList = $this->getMock(
+        $this->dataProvider = $this->getMock(
             'JelmerSnoeck\KataEight\Extendible\DataProvider'
         );
     }
 
     public function test_it_stores_data_list_object()
     {
-        $processor = new Processor($this->dataList);
-        $this->assertSame($this->dataList, $processor->getDataProvider());
+        $processor = new Processor($this->dataProvider);
+        $this->assertSame($this->dataProvider, $processor->getDataProvider());
     }
 
     public function test_it_loads_list_from_data_list()
     {
         $length = 6;
 
-        $this->dataList->expects($this->once())
+        $this->dataProvider->expects($this->once())
             ->method('loadList')->with($length);
 
-        $processor = new Processor($this->dataList);
+        $processor = new Processor($this->dataProvider);
         $processor->loadList($length);
     }
 
     public function test_it_gets_word_list()
     {
-        $this->dataList->expects($this->once())
+        $this->dataProvider->expects($this->once())
             ->method('getWordList')->will($this->returnValue('wordList'));
 
-        $processor = new Processor($this->dataList);
+        $processor = new Processor($this->dataProvider);
         $this->assertSame('wordList', $processor->getWordList());
     }
 
     public function test_it_gets_valid_words()
     {
-        $this->dataList->expects($this->once())
+        $this->dataProvider->expects($this->once())
             ->method('getValidWords')->will($this->returnValue('validWords'));
 
-        $processor = new Processor($this->dataList);
+        $processor = new Processor($this->dataProvider);
         $this->assertSame('validWords', $processor->getValidWords());
     }
 
     public function test_it_gets_words_for_length()
     {
         $length = 6;
-        $this->dataList->expects($this->once())
+        $this->dataProvider->expects($this->once())
             ->method('getWordsForLength')->with($length)
             ->will($this->returnValue('wordsForLength'));
 
-        $processor = new Processor($this->dataList);
+        $processor = new Processor($this->dataProvider);
         $this->assertSame(
             'wordsForLength',
             $processor->getWordsForLength($length)
@@ -78,16 +78,16 @@ class ProcessorTest extends \PHPUnit_Framework_TestCase
 
     public function test_it_combines_set_of_valid_words()
     {
-        $this->dataList->expects($this->once())->method('getWordsForLength')
+        $this->dataProvider->expects($this->once())->method('getWordsForLength')
             ->with(4)->will($this->returnValue(array(
                 'bums' => true, 'here' => true,
             )));
-        $this->dataList->expects($this->once())->method('getValidWords')
+        $this->dataProvider->expects($this->once())->method('getValidWords')
             ->will($this->returnValue(array(
                 'albums' => true, 'hereby' => true,
             )));
 
-        $processor = new Processor($this->dataList);
+        $processor = new Processor($this->dataProvider);
         $processor->loadList(6);
 
         $combinedWords = $processor->createValidWordsForList(
